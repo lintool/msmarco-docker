a6b29f89bd928394610047761306728187ad8fc9
lintool <jimmylin@uwaterloo.ca>
2021-01-04 15:29:45 -0500

    Pyserini Docker, MS MARCO doc, w/ expansion, per doc.

---
+++ pyserini-msmarco-doc-expanded-per-doc/main.py
@@ -0,0 +1,24 @@
+import sys
+
+# Only for debugging purposes, using a Pyserini local installation.
+# sys.path.insert(0, '../pyserini/')
+
+from fastapi import FastAPI
+from pyserini.search import SimpleSearcher
+from typing import Optional
+
+
+searcher = SimpleSearcher.from_prebuilt_index('msmarco-doc-expanded-per-doc')
+searcher.set_bm25(4.68, 0.87)
+app = FastAPI()
+
+
+@app.get("/search/")
+def search(q: str, k: Optional[int] = 1000):
+    hits = searcher.search(q, k=k)
+
+    results = []
+    for i in range(0, len(hits)):
+        results.append({'docid': hits[i].docid, 'score': hits[i].score})
+
+    return {'results': results}
